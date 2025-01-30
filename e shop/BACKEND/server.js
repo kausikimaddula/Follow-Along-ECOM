482764fb34b93a730c60fde0a17f709dee6ede47
kausikimaddula <saikausikimaddula80@gmail.com>
2025-01-30 09:18:10 +0530

    done

--- e shop/BACKEND/server.js
+++ e shop/BACKEND/server.js
@@ -1,13 +1,14 @@
+const {connect,connection}=require("mongoose")
 const{app} = require("./app")
 require("dotenv").config()
 
-
+const Connect=require("./db/connection")
 const port = process.env.PORT
 app.get("/test",async(req,res)=>{
-
     res.send("hello.....")
-
 })
+
+
 
 app.listen(port,async()=>{
     try{
